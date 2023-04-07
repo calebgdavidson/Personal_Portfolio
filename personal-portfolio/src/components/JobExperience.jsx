ad9f03e79ec2205f44ae60a7bf51652a4f84cd27
Caleb Davidson <caleb.davidson@michaelkors.com>
2023-04-07 12:56:52 -0500

    navbar fix

--- personal-portfolio/src/components/JobExperience.jsx
+++ personal-portfolio/src/components/JobExperience.jsx
@@ -59,7 +59,7 @@ style: "shadow-blue-400",
 ];
 
 return (
-  <div name="experience" className="bg-black w-full h-screen h-full">
+  <div name="technologies" className="bg-black w-full h-screen h-full">
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full text-white">
         <div>
           <p className="text-6xl font-bold border-b-4 border-gray-500 p-2 inline">
